7bee313d3b6a1a7a82e3b635a1d838477f58a402
Knight <mayanks365@gmail.com>
2020-11-25 22:28:01 +0530

    practice

--- oop/planner/assets/scripts/app.js
+++ oop/planner/assets/scripts/app.js
@@ -1,7 +1,7 @@
 class DOMHelper {
-  static clearEventListners( element ){
+  static clearEventListners( element ){ // element -> button
     const clonedElement = element.cloneNode( true );
-    element.repalceWith( clonedElement );
+    element.replaceWith( clonedElement );
     return clonedElement;
   }
 
@@ -13,69 +13,82 @@ class DOMHelper {
 }
 
 class Tooltip {
+
 }
 
+
 class ProjectItem {
+  //             | 3 - EVENT switchPROJECT(ProjectItem) |
   constructor( id, updateProjectListsFunction, type ) {
     this.id = id;
-    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.updateProjectListsHandler = updateProjectListsFunction; // 4 Event
     this.connectMoreInfoButton();
     this.connectSwitchButton(type)
   }
 
   connectMoreInfoButton() {
+
   };
-  
+
   connectSwitchButton( type ){
     const projectItemElement = document.getElementById( this.id );
 
     let switchBtn = projectItemElement.querySelector( 'button:last-of-type'  );
-    // switchBtn = DOMHelper.clearEventListner(switchBtn);
-    switchBtn.textContent = type === 'active' ? 'Finish' : 'Active';
+    switchBtn = DOMHelper.clearEventListners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
     switchBtn.addEventListener( 'click', 
-      this.updateProjectListsHandler.bind( null, this.id ) )
+    // 5 - EVENT                         ProjectItem | id
+      this.updateProjectListsHandler.bind( null, this.id ) ) // null argument will be kept on hold
   }
 
+  //      switchProjects()
   update( updateProjectListsFn, type ){
-    this.updateProjectListsHandler = updateProjectListsFn;
+    this.updateProjectListsHandler = updateProjectListsFn; // null will take place switchProject()
     this.connectSwitchButton(type);
   }
 }
 
-class ProjectList {
+class ProjectList { 
   projects = [];
 
-  constructor( type ) { 
-    this.type = type; // type -> active / finished
+  constructor( type ) { // type -> active / finished
+    this.type = type; 
 
     // select li of id="active-projects li"/ id="finished-projects li"
     const prjItems = document.querySelectorAll( `#${type}-projects li` );
     // console.log(prjItems);
     for ( const prjItem of prjItems ) {
-      this.projects.push( 
-        new ProjectItem( prjItem.id, this.switchProjects.bind(this), this.type ) );
+      this.projects.push( // id   |1- EVENT ON HOLD switchProject()|active/finished
+        new ProjectItem( prjItem.id, this.switchProject.bind(this), this.type ) );
     }
     console.log( this.projects );
-  }
+  } 
 
+  //                           addProject()
   setSwitchHandlerFunction( switchHandlerFunction ){
     this.switchHandler = switchHandlerFunction;
   }
 
-  addProjects( project ){
-    this.projects.push( project );
+  addProjects( project ){ 
+    //      p3        | p1 , p2
+    //      p1 , p2   | p3
+    this.projects.push( project ); 
     DOMHelper.moveElement( project.id, `#${this.type}-projects ul`);
-    projects.update( this.switchProject.bind(this), this.type );
+    project.update( this.switchProject.bind(this), this.type ); // finished | active
   }
 
-  switchProjects( projectId ){
+  // 2- PART OF EVENT
+  switchProject( projectId ){
     // const projectIndex = this.projects.findIndex( p => { p.id === projectId });
     // this.projects.splice(projectIndex, 1);
+
+    //   addProjects()
     this.switchHandler( this.projects.find( p => p.id === projectId ) );
     // shorter code with filter
     // return/keep item when condition is true
     this.projects = this.projects.filter( p => p.id !== projectId );   
 
+    console.log(this.switchHandler)
   }
 }
 
@@ -103,3 +116,26 @@ App.init();
     // Array.prototype.findIndex() expects a callback as first parameter. Use 
     // this if you need the index in arrays with non-primitive types (e.g.
     // objects) or your find condition is more complex than just a value.
+
+
+// textContent -> The textContent property sets or returns the text content of the specified node, 
+// and all its descendants.
+// const projectItemElement = document.querySelector( '#active-projects' );
+// const content = projectItemElement.querySelector( 'button' )
+// content.textContent = 'na ho paega'
+// console.log(content.textContent)
+
+// https://www.w3schools.com/jsref/met_node_clonenode.asp
+// cloneNode
+// Copy a <li> element from one list to another:
+
+// https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/replaceWith
+// The ChildNode.replaceWith() 
+// method replaces this ChildNode in the children list of its parent with a set of Node 
+// var parent = document.createElement("div");
+// var child = document.createElement("p");
+// child.textContent = 'na ho paega'
+// var span = document.createElement("span");
+// child.replaceWith(span);
+// const teex = span.textContent = 'fir bhi na ho paega'
+// console.log(teex)
--- oop/planner/callBack/apps.js
+++ oop/planner/callBack/apps.js
@@ -36,17 +36,17 @@
 
 //     setSwitchHandlerFunction( switchHandlerFunction ) {  // 9 
 //         // ProjectsList.addProject.bind(finishedProjectsList) 
-//         this.
-//  = switchHandlerFunction; // 8
+//         this.switchHandler = switchHandlerFunction; // 8
 //     }
 
 //     addProject() {
 //         console.log('two')
 //         console.log(this) // finishedProjectsList
+//         console.log(this.type)
 //         console.log('four')
 //     }
 
-//     switchProject( projectId ){ // 5
+//     switchProject( projectId ) { // 5
 //         console.log('one')
 //         this.switchHandler( this.projects.find( p => p.id === projectId ) ) // 7
 //         console.log('five')
@@ -76,7 +76,8 @@
 
 class c {
 
-    constructor( updateProjectListsFunction ){ // three
+    constructor( updateProjectListsFunction, id='daddu' ){ // three
+        this.id = id
         this.updateProjectListsHandler = updateProjectListsFunction; // two
         this.connectSwitchButton();
     }
@@ -84,47 +85,66 @@ class c {
         connectSwitchButton() {
         const projectItemElement = document.getElementById('active-projects');
         const switchBtn = projectItemElement.querySelector('button:last-of-type')
+        const ID = projectItemElement.getAttribute('id')
         switchBtn.addEventListener( 'click', 
-        this.updateProjectListsHandler ) // one
+        this.updateProjectListsHandler.bind( null, ID, this.id  ) ) // one
     }
-
 }
 
 class b {
 
     projects = [1,2,3,4]; 
-    constructor(){
+    constructor(type , ppp){
+        this.type = type // c
+        this.ppp = ppp
 
-        const d = new c( this.switchProject.bind(this) );
+        const d = new c( this.switchProject.bind(this) ); // four
         d
         // this.switchProject;
     }
 
     setSwitchHandlerFunction( switchHandlerFunction ) {  // 9 
-        this.switchHandler = switchHandlerFunction; // 8
+        this.switchHandler = switchHandlerFunction; // 8 addProject.bind( b )
+        // console.log(this)
         // this.switchProject();
     }
 
     addProject(){
-        console.log(this)
-        console.log(this.find( p =>  p > 2 ))
+        console.log( this ) //seven
+        console.log( this.type ) // d
+        console.log( this.projects.find( p =>  p > 2 ) ) // eight
     }
 
-    switchProject(){
+    switchProject( ID, id ){
         // this.addProject();
-        console.log( this.projects.find( p =>  p > 3 ) );
-        console.log( this.switchHandler( this ) )
+        console.log( ID ) // five
+        console.log( id ) // five
+        console.log( this.projects.find( p =>  p > 3 ) ); // six
+        // this.projects.find( p => p === 1 )
+        this.switchHandler( console.log( this.ppp ) )   // nine
+        // trigger addProject( )
     }
 }
 
-class a {
-    static init(){
-        const pr = [4,5,6,7,8,9];
 
-        const c = new b( );
-        c.setSwitchHandlerFunction( c.addProject.bind( pr ) );
+class a {
+
+    pr = [4,5,6,7,8,9];
+
+    chappa(){
+        console.log(this.pr)
+    }
+
+    static init(){
+        const ppp = [9098,9868]
+        const c = new b( 'lalla',  ppp ); //a
+        c.setSwitchHandlerFunction( c.addProject.bind( c ) ); // b
         // c.addProject();   
     }
 }
 
 a.init();
+
+const l =  new a ();
+console.log( l.pr );
+l.chappa();
