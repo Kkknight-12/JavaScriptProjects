45a8d28adb0cc868d4ec6a771f51f259fda73e62
Knight <mayanks365@gmail.com>
2020-11-28 12:37:17 +0530

    small changes made

--- oop/planner/bluePrint/cloneNode/app.js
+++ oop/planner/bluePrint/cloneNode/app.js
@@ -1,38 +1,38 @@
 class DOMHelper {
-  static clearEventListeners( element ){ // element -> button
-    const clonedElement = element.cloneNode( true );
-    element.replaceWith( clonedElement );
-    return clonedElement;
-  }
+    static clearEventListeners( element ){ // element -> button
+        const clonedElement = element.cloneNode( true );
+        element.replaceWith( clonedElement );
+        return clonedElement;
+    }
 
-  static moveElement( elementId, newDestinationSelector ){
-    const element = document.getElementById( elementId );
-    const destinationElement = document.querySelector(newDestinationSelector);
-    destinationElement.append(element)
-  }
+    static moveElement( elementId, newDestinationSelector ){
+        const element = document.getElementById( elementId );
+        const destinationElement = document.querySelector(newDestinationSelector);
+        destinationElement.append(element)
+    }
 }
 
 class ProjectItem {
-    constructor( id, updateProjectListsFunction , type ){ // three
+    constructor( id, updateProjectListsFunction  ){ // three
         this.id = id;
         this.updateProjectListsHandler = updateProjectListsFunction; // two
-        this.connectSwitchButton(type);
+        this.connectSwitchButton();
         
     }
 
     connectSwitchButton(){
-        const projectItemElement = document.getElementById(this.id);
-        let switchBtn = projectItemElement.querySelector('button:first-of-type')
+        const projectItemElement = document.getElementById( this.id );
+        let switchBtn = projectItemElement.querySelector( 'button:first-of-type' )
         switchBtn = DOMHelper.clearEventListeners(switchBtn);
         switchBtn.addEventListener( 'click', 
         this.updateProjectListsHandler.bind(null, this.id) ) // one
     }
 
-     //      switchProjects()
-  update( updateProjectListsFn ){
-    this.updateProjectListsHandler = updateProjectListsFn; // null will take place switchProject()
-    this.connectSwitchButton();
-  }
+    //      switchProjects()
+    update( updateProjectListsFn ){
+        this.updateProjectListsHandler = updateProjectListsFn; // null will take place switchProject()
+        this.connectSwitchButton();
+    }
 }
 
 class ProjectList {
@@ -55,14 +55,14 @@ class ProjectList {
         this.switchHandler = switchHandlerFunction; // 8
     }
 
-    addProject(project){
+    addProject(project) {
         // inside function finishedProjectsList.addProject
         this.projects.push(project)
         DOMHelper.moveElement( project.id, `#${this.type}`);
         project.update( this.switchProject.bind(this), this.type );
     }
 
-    switchProject( projectId ){ // 5s
+    switchProject( projectId ) { // 5s
         // outside function activeProjectsList.setSwitchHandlerFunction 
         this.switchHandler( this.projects.find( p => p.id === projectId ) ) // 7
         this.projects = this.projects.filter( p => p.id !== projectId );
@@ -70,7 +70,7 @@ class ProjectList {
 }
 
 class App {
-    static init(){
+    static init() {
         const List1 = new ProjectList( 'myList1' );
         const List2 = new ProjectList( 'myList2' );
 
