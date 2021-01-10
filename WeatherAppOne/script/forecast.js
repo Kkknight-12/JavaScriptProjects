1130f44153aa5afe095f464549e34c107722c42b
Knight <mayanks365@gmail.com>
2021-01-10 21:14:51 +0530

    updating weather app

--- WeatherAppOne/script/forecast.js
+++ WeatherAppOne/script/forecast.js
@@ -1,5 +1,16 @@
 const key = 't9legpG9USnQv7mzkPz6kTC1npNtdhWp';
 
+
+const getWeather = async (locationId) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationId}?apikey=${key}`;
+
+    const response = await fetch( base + query );
+    const data = await response.json();
+
+    return data[0];
+}
+
 const getCity = async (city) =>{
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +21,9 @@ const getCity = async (city) =>{
     return data[0]
 }
 
-getCity('bhopal')
-    .then( (data) => { console.log(data) } )
-    .catch( (err) => { console.log(err) } );
+// getCity('bhopal')
+//     .then( (data) => { console.log(data) } )
+//     .catch( (err) => { console.log(err) } );
+
+// getWeather('204408')
+//     .then( (data) => console.log(data))
